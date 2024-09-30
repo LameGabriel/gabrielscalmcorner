50ad616a7947fd3a80a98647672d12e0eab5d542
LameGabriel <105407739+LameGabriel@users.noreply.github.com>
2024-09-29 21:06:35 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// Add event listener to fade out and redirect to the main page
-document.getElementById("main-page").addEventListener("click", function() {
-    // Fade out the page by setting the opacity to 0
-    document.getElementById("main-page").style.opacity = '0';
+const mainPage = document.getElementById("main-page");
 
-    // Wait for the fade-out animation to complete before redirecting (2 seconds)
+mainPage.addEventListener("click", function() {
+    mainPage.style.opacity = '0';
+
     setTimeout(function() {
-        window.location.href = "Main.html"; // Replace with your actual next page URL
-    }, 2000); // 2000ms delay matches the CSS transition duration
+        window.location.href = "Main.html"; 
+    }, 2000); 
 });
-
