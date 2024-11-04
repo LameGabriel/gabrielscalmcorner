20a2140ea48334d90a6fe9ca195ea35113b8a2d1
LameGabriel <105407739+LameGabriel@users.noreply.github.com>
2024-11-03 22:03:36 -0700

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const mainPage = document.getElementById("main-page");
-
-mainPage.addEventListener("click", function() {
-    mainPage.style.opacity = '0';
-
-    setTimeout(function() {
-        window.location.href = "Main.html"; 
-    }, 2000); 
-});
